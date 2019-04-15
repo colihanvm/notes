1a8411d67f3360a65f4f6e35ba62e57b8d97745a
colihanvm <vanessa.colihan@gmail.com>
2019-04-15 16:50:44 -0400

    update to script

--- extract_script.m
+++ extract_script.m
@@ -1,10 +1,30 @@
 close all
+%set input values
 radius = 5.4;
 num_find = 19;
 threshold = 155;
 
+%read notes from different sheets of music
 im = imread('img/rowrowbt_mod3.png');
 row_notes = extract_notes(im, threshold, num_find, radius);
 
 im = imread('img/lond_mod2.jpg');
 lond_notes = extract_notes(im, threshold, num_find, radius);
+
+%write to a .txt file
+% f = fopen('test1.txt', 'w');
+% formatSpec = '%c%c\n';
+% fprintf(f, formatSpec, lond_notes');
+
+%write to phone
+% s = serial('cu.usbmodem1411');
+% s = fopen(s);
+% formatSpec = '%c%c\n';
+% fprintf(s, formatSpec, lond_notes');
+% fclose(s);
+
+s = serial('cu.usbmodem1411');
+fopen(s);
+fprintf(s, '*IDN?');
+idn = fscanf(s);
+fclose(s);
